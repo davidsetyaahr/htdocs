121e7dc6e9341cd64c7e6e00ea755ccd7dd36162
davidsetyaahr <davidsetya24@gmail.com>
2019-05-07 09:43:10 +0700

    update menu

---
+++ msc/application/views/data-siswa/list-siswa.php
@@ -0,0 +1,28 @@
+<div class="row">
+	<div class="col-lg-12">
+		<div class="table-responsive">
+			<table class="table table-striped table-bordered table-hover">
+				<thead>
+					<tr>
+						<th>Kode Siswa</th>
+						<th>Nama Siswa</th>
+						<th>Tanggal Lahir</th>
+						<th>Jenis Kelamin</th>
+						<th>Alamat</th>
+						<th>No Telepon Siswa</th>
+						<th>Grup</th>
+						<th>Nama Orang Tua</th>
+						<th>No Telepon Wali</th>
+						<th>Tanggal Pendaftaran</th>
+						<th>Opsi</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
