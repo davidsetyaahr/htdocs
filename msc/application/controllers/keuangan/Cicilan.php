9695851aac0512cd204a4f247810a8ad8bb774ba
[indrinurhamida] <[indrihamida7@gmail.com]>
2019-05-10 13:45:50 +0700

    tambah cicilan

---
+++ msc/application/controllers/keuangan/Cicilan.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cicilan extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->title = $this->common_lib->getTitle();
+	}
+
+	public function index()
+	{
+       $menu = array(
+            "title" => $this->title,
+			"btnHref" => base_url()."keuangan/cicilan/list-cicilan",
+			"btnBg" => "success",
+			"btnFa" => "keyboard",
+			"btnText" => "Tambah Data"
+		);
+		$card['title'] = "Cicilan <span>> List Cicilan</span>";
+       // $data["data"] = $this->common->getData("*", "", "", "", "");
+		$this->load->view('common/menu', $menu);
+		$this->load->view('common/card', $card);
+		$this->load->view('keuangan/cicilan/list-cicilan');
+		$this->load->view('common/slash-card');
+        $this->load->view('common/footer');
+        
+	}
+
+	public function input_cicilan()
+	{
+		$this->load->view('list-cicilan');
+    }
+}
