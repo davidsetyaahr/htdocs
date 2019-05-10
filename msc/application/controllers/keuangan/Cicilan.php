fe0c1ebb709c44a8a96d074c462546eca02bcd40
[indrinurhamida] <[indrihamida7@gmail.com]>
2019-05-10 14:29:07 +0700

    input dan list cicilan

--- msc/application/controllers/keuangan/Cicilan.php
+++ msc/application/controllers/keuangan/Cicilan.php
@@ -12,10 +12,10 @@ class Cicilan extends CI_Controller {
 	{
        $menu = array(
             "title" => $this->title,
-			"btnHref" => base_url()."keuangan/cicilan/list-cicilan",
+			"btnHref" => base_url()."keuangan/cicilan/input_cicilan",
 			"btnBg" => "success",
 			"btnFa" => "keyboard",
-			"btnText" => "Tambah Data"
+			"btnText" => "Tambah Cicilan"
 		);
 		$card['title'] = "Cicilan <span>> List Cicilan</span>";
        // $data["data"] = $this->common->getData("*", "", "", "", "");
@@ -29,6 +29,18 @@ class Cicilan extends CI_Controller {
 
 	public function input_cicilan()
 	{
-		$this->load->view('list-cicilan');
+		$menu = array(
+			"title" => $this->title,
+			"btnHref" => base_url()."keuangan/cicilan",
+			"btnBg" => "primary","btnFa" => "keyboard",
+			"btnText" => "List Cicilan"
+		);
+		$card['title'] = "Cicilan <span>> Tambah Cicilan </span>";
+		//$data["data"] = $this->common->getData("*", "mapel", "", "", "");
+		$this->load->view('common/menu', $menu);
+        $this->load->view('common/card', $card);
+		$this->load->view('keuangan/cicilan/tambah-cicilan');
+		$this->load->view('common/slash-card');
+        $this->load->view('common/footer');
     }
 }
--- msc/application/views/keuangan/cicilan/list-cicilan.php
+++ msc/application/views/keuangan/cicilan/list-cicilan.php
@@ -1,36 +1,32 @@
-<form action="<?php echo base_url()."keuangan/cicilan/input_cicilan" ?>" method="post">
-	<div class="row">
-		<div class="col-lg-6">
-			<label>Kode Cicilan</label>
-			<input type="text" class="form-control" name="">
-		</div>
-		<div class="col-lg-6">
-			<label>Kode Siswa</label>
-			<input type="text" class="form-control" name="kode_siswa">
-		</div>
-		<div class="col-lg-6">
-			<label>Nama Siswa</label>
-			<input type="text" class="form-control" name="nama_siswa" value="">
-		</div>
-		<div class="col-lg-6">
-			<br>
-			<label>Biaya/cicilan</label>
-			<input type="number" class="form-control" name="" value="">
-		</div>
-		<div class="col-lg-6">
-			<br>
-			<label>Jumlah Cicilan</label>
-			<input type="number" class="form-control" name="" value="">
-		</div>
-		<div class="col-lg-6">
-			<br>
-			<label>Total Biaya</label>
-			<textarea type="number" class="form-control" name="" ></textarea>
-		</div>
-
-		<div class="col-lg-6">
-			<br>
-			<?php $this->load->view("common/btn") ?>
+<div class="row">
+	<div class="col-lg-12">
+		<div class="table-responsive">
+			<table class="table table-striped table-bordered table-hover">
+				<thead>
+					<tr>
+						<th>Kode Cicilan</th>
+						<th>Nama Siswa</th>
+						<th>Total Biaya</th>
+						<th>Jumlah Cicilan</th>
+						<th>Biaya/cicilan</th>
+						<th>Sisa Cicilan</th>
+						<th>Sisa Biaya</th>
+						<th>Opsi</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
-</form>
+</div>
